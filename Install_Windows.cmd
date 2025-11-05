99214e5b1474215a47598bd5ca58067a06ac24c4
Alexey Frolov <frolovlife@users.noreply.github.com>
2025-11-05 22:20:45 +0400

    Add files via upload

--- Install_Windows.cmd
+++ Install_Windows.cmd
@@ -5,32 +5,24 @@ title Midnight theme for OBS Studio installation
 echo Midnight theme for OBS Studio installation
 echo.
 
-if exist "%AppData%\obs-studio\themes\" (
-	echo Theme directory exists.
+if not exist "%AppData%\obs-studio\themes\" (
+	echo Creating themes directory for the first time...
+	md "%AppData%\obs-studio\themes\"
 	echo.
-
-	if exist "%AppData%\obs-studio\themes\Midnight.obt" (
-		echo Previos version of Midnight theme exists.
-		echo Deleting previos version...
-		rd /s /q "%AppData%\obs-studio\themes\HollowDark\"
-		del /q "%AppData%\obs-studio\themes\Midnight*.o?t"
-		echo.
-	) else (
-		rem hello world
-	)
-
-	echo Copying theme files...
-	xcopy /e /i /f /y "%cd%\Midnight\*" "%AppData%\obs-studio\themes\"
-	echo.
-	echo Installation has completed!
-	echo Restart OBS Studio, then open OBS Studio settings and set the Midnight theme.
-) else (
-	echo Theme directory not found. OBS Studio is probably not installed.
-	echo Install OBS Studio first, then install the theme.
-	echo.
-	echo Installation aborted.
 )
 
+if exist "%AppData%\obs-studio\themes\Midnight.obt" (
+	echo Removing a previos version of Midnight theme...
+	rd /s /q "%AppData%\obs-studio\themes\HollowDark\"
+	del /q "%AppData%\obs-studio\themes\Midnight*.o?t"
+	echo.
+)
+
+echo Copying theme files...
+xcopy /e /i /f /y "%cd%\Midnight\*" "%AppData%\obs-studio\themes\"
+echo.
+echo Installation has completed!
+echo Restart OBS Studio, then open OBS Studio settings and set the Midnight theme.
 echo.
 echo Press any key to exit...
 pause > nul
