d5cf181cb8c899962e5c57b254d4fd3dd924d837
Alexey Frolov <frolovlife@users.noreply.github.com>
2025-11-05 18:32:03 +0400

    Add files via upload

--- Install_Windows.cmd
+++ Install_Windows.cmd
@@ -2,17 +2,24 @@
 title Midnight theme for OBS Studio installation
 @setlocal enableextensions
 @cd /d "%~dp0"
-
-echo Checking for theme directory...
-mkdir "%AppData%\obs-studio\themes\"
+echo Midnight theme for OBS Studio installation
 echo.
 
-echo Copying theme files...
-xcopy /e /i /f /y "%cd%\Midnight\*" "%AppData%\obs-studio\themes\"
-echo.
+if exist "%AppData%\obs-studio\themes\" (
+	echo Theme directory exists.
+	echo.
+	echo Copying theme files...
+	xcopy /e /i /f /y "%cd%\Midnight\*" "%AppData%\obs-studio\themes\"
+	echo.
+	echo Installation has completed!
+	echo Restart OBS Studio, then open OBS Studio settings and set the Midnight theme.
+) else (
+	echo Theme directory not found. OBS Studio is probably not installed.
+	echo Install OBS Studio first, then install the theme.
+	echo.
+	echo Installation aborted.
+)
 
-echo Installation has completed!
-echo Restart OBS Studio, then open OBS Studio settings and set the Midnight theme.
 echo.
 echo Press any key to exit...
 pause > nul
