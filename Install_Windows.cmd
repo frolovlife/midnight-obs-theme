75ed78501b382767b3f41b146e4e4986b14a83d6
Alexey Frolov <frolovlife@users.noreply.github.com>
2025-11-06 00:59:42 +0400

    Update Install_Windows.cmd

--- Install_Windows.cmd
+++ Install_Windows.cmd
@@ -19,10 +19,11 @@ if exist "%AppData%\obs-studio\themes\Midnight.obt" (
 )
 
 echo Copying theme files...
-xcopy /e /i /f /y "%cd%\Midnight\*" "%AppData%\obs-studio\themes\"
+xcopy /e /i /q /y "%cd%\Midnight\*" "%AppData%\obs-studio\themes\"
 echo.
 echo Installation has completed!
 echo Restart OBS Studio, then open OBS Studio settings and set the Midnight theme.
 echo.
 echo Press any key to exit...
 pause > nul
+
